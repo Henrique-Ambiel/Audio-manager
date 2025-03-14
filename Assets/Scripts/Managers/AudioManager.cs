1936b1bad5c3a9612d0d4de5775b9f5918edacbd
Henrique Ambiel <henriqueambiel@gmail.com>
2025-03-14 19:53:15 -0300

    Initial commit

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,14 @@
+
+[System.Serializable]
+public struct AudioCue
+{
+    public string name;
+    public AudioClip clip;
+    public float volume;
+    public bool loop;
+}
+
+public class AudioManager
+{
+    
+}
